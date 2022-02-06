8441b394925e52760e8cd978470274febdd2752d
Jake <jake.k.hassel@gmail.com>
2022-02-05 23:42:41 -0500

    Add tailwind and chartkick

---
+++ app/src/components/simple-chart.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const data = ref([["Jan", 4], ["Feb", 2], ["Mar", 10], ["Apr", 5], ["May", 3], ["Jun", 8], ["Jul", 4]])
+</script>
+<template>
+  <div>
+    <line-chart :data="data" />
+  </div>
+</template>
--- app/src/main.ts
+++ app/src/main.ts
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
+import VueChartkick from 'vue-chartkick'
+import 'chartkick/chart.js'
 import App from './App.vue'
+import './index.css'
 
-createApp(App).mount('#app')
+
+createApp(App).use(VueChartkick).mount('#app')
