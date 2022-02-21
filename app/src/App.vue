1023c80d05566cb5f31e44cf8d1829ca66602ef4
Jake <jake.k.hassel@gmail.com>
2022-02-20 20:54:33 -0500

    Using tailwind font

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,18 +1,5 @@
-<script setup lang="ts">
-import WorldMap from "./components/world-map.vue";
-import PopulationGlobe from "./components/population-globe.vue";
-</script>
-
 <template>
   <main>
     <router-view />
   </main>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-</style>
