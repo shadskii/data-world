447690ffdc93f05c5aa1aefcbb2ed126a3bb23b4
Jake <jake.k.hassel@gmail.com>
2022-03-08 23:21:34 -0500

    Configuring for deployment

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,5 +1,5 @@
 import cubejs from "@cubejs-client/core";
 
 export const cubejsApi = cubejs(import.meta.env.VITE_CUBEJS_TOKEN, {
-  apiUrl: `http://localhost:4000/cubejs-api/v1`,
+  apiUrl: import.meta.env.VITE_CUBEJS_URL,
 });
