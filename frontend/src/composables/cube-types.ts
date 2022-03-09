447ab50091135dec19dd8790552d13ff67ef3443
Jake <jake.k.hassel@gmail.com>
2022-03-08 22:50:15 -0500

    Add docs to cube types

--- frontend/src/composables/cube-types.ts
+++ frontend/src/composables/cube-types.ts
@@ -6,22 +6,68 @@ import { Sex } from "../stores/detailed-population-data";
  * This must be kept in sync with the cube.js schema.
  */
 export interface CubeDimensions {
+  /**
+   * Fips country code.
+   */
   "PredictedPopulation.country": string;
+
+  /**
+   * Year of the population data.
+   */
   "PredictedPopulation.year": string;
+
+  /**
+   * The total population of the world.
+   */
   "PredictedPopulation.totalPopulation": number;
 
   "DetailedPopulation.key": string;
+
+  /**
+   * Age/sex specific population.
+   */
   "DetailedPopulation.population": number;
+
+  /**
+   * Population sex.
+   */
   "DetailedPopulation.sex": Sex;
+
+  /**
+   * Population age.
+   */
   "DetailedPopulation.age": number;
+
+  /**
+   * Fips country code.
+   */
   "DetailedPopulation.country": string;
+
+  /**
+   * Year of population data.
+   */
   "DetailedPopulation.year": string;
 
   "MortalityLifeExpectancy.key": string;
+
+  /**
+   * Life expectancy in years.
+   */
   "MortalityLifeExpectancy.lifeExpectancy": number;
+
+  /**
+   * Infant mortality rate in percent.
+   */
   "MortalityLifeExpectancy.infantMortality": number;
 
+  /**
+   * Fips country code.
+   */
   "Areas.country": string;
+
+  /**
+   * Country area in square kilometers.
+   */
   "Areas.area": number;
 }
 
@@ -31,8 +77,33 @@ export interface CubeDimensions {
  * This must be kept in sync with the cube.js schema.
  */
 export interface CubeMeasures {
+  /**
+   * Total males.
+   */
   "DetailedPopulation.totalMales": number;
+
+  /**
+   * Total females.
+   */
   "DetailedPopulation.totalFemales": number;
 
+  /**
+   * Total number of countries.
+   */
   "PredictedPopulation.numberOfCountries": number;
 }
+
+export type DetailedPopulation = {
+  country: string;
+  dimensions: {
+    country: string;
+    popilation: number;
+    sex: Sex;
+    age: number;
+    year: string;
+  };
+  measrures: {
+    totalMales: number;
+    totalFemales: number;
+  };
+};
